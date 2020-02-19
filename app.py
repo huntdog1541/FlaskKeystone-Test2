5057160c5cc91f066f56864b048c694905d9b1e9
David Hunt <huntdog1541@gmail.com>
2020-02-19 22:31:19 +0000

    Upgrading project2

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+from keystone import *
 
 app = Flask(__name__)
+
+def test_ks(arch, mode, code, syntax=0):
+    ks = Ks(arch, mode)
+    if syntax != 0:
+        ks.syntax = syntax
+    
+    encoding, count = ks.asm(code)
+
+    print("%s = [ " % code)
+    for i in encoding:
+        print("%02x " % i)
+    print("]")
 
 
 @app.route('/')
 def hello_world():
+    test_ks(KS_ARCH_X86, KS_MODE_16, b"add eax, ecx")
     return 'Hello World!'
 
 
